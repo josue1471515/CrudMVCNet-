74010d36faf95523c1fe61aba72e6f2ea61ff585
josue <josue.ferrufino.soft@gmail.com>
2019-05-17 21:44:42 -0400

    Base

---
+++ crudNetMvc/Controllers/PersonsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crudNetMvc.Models;
+using crudNetMvc.Models.ViewModels;
+
+namespace crudNetMvc.Controllers
+{
+    public class PersonsController : Controller
+    {
+        // GET: Persons
+        public ActionResult Index()
+        {
+            List<ListPersonViewModel> lst;
+            using (crud1Entities1 db = new crud1Entities1()) {
+                 lst = (from d in db.Persons
+                           select new ListPersonViewModel
+                           {
+                               PersonID = d.PersonID,
+                               LastName = d.LastName,
+                               FirstName = d.FirstName,
+                               Address = d.Address,
+                               City = d.City
+                           }).ToList();
+                            
+            }
+                return View(lst);
+        }
+
+       
+
+       
+    
+
+        public ActionResult UpdatePerson()
+        {
+            return View();
+        }
+
+        public ActionResult CreatePerson() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(PersonViewModel model) {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (crud1Entities1 db = new crud1Entities1())
+                    {
+                        var oPerson = new Persons();
+                        oPerson.LastName = model.LastName;
+                        oPerson.FirstName = model.FirstName;
+                        oPerson.Address = model.Address;
+                        oPerson.City = model.City;
+                        db.Persons.Add(oPerson);
+                        db.SaveChanges();
+                    }
+                }
+                return Redirect("~/Persons/Index");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult EditPerson( int PersonID)
+        {
+            PersonViewModel person = new PersonViewModel();
+            using (crud1Entities1 db = new crud1Entities1()) {
+                var oTable = db.Persons.Find(PersonID);
+                person.FirstName = oTable.FirstName;
+                person.LastName = oTable.LastName;
+                person.Address = oTable.Address;
+                person.City = oTable.City;
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult EditPerson(PersonViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (crud1Entities1 db = new crud1Entities1())
+                    {
+                        var oPerson = db.Persons.Find(model.PersonID);
+                        oPerson.LastName = model.LastName;
+                        oPerson.FirstName = model.FirstName;
+                        oPerson.Address = model.Address;
+                        oPerson.City = model.City;
+                        db.Entry(oPerson).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                return Redirect("~/Persons/Index");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+
+        public ActionResult DeletePerson(int PersonID)
+        {
+            PersonViewModel person = new PersonViewModel();
+            using (crud1Entities1 db = new crud1Entities1())
+            {
+                var oTable = db.Persons.Find(PersonID);
+                db.Persons.Remove(oTable);
+                db.SaveChanges();
+            }
+            return Redirect("~/Persons/");
+        }
+
+    }
+}
---
+++ crudNetMvc/Models/ViewModels/PersonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace crudNetMvc.Models.ViewModels
+{
+    public class PersonViewModel
+    {
+        public int PersonID { get; set; }
+        [Required]
+        [Display(Name = "Apellido")]
+        [StringLength(20)]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Nombres")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Direccion")]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Ciudad")]
+        public string City { get; set; }
+    }
+}
